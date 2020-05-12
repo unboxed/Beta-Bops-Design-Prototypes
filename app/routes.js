81be9bd11af67b3f28e2c517bd8de250681a0085
claudia-hop <claudia.i.hopkins@gmail.com>
2020-05-12 16:19:31 +0100

    Branching decision so manager can disagree and request amendment from officer

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+
+// Branching
+router.post('/approve-recomendation-answer', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let over18 = req.session.data['over-18']
+
+  if (over18 === 'false') {
+    res.redirect('v05/approve-recommendation-no')
+  } else {
+    res.redirect('v05/approve-recommendation-yes')
+  }
+})
 
 module.exports = router
