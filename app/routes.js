5505e015e316b4372f97d872d9b2fd51729e2fa5
claudia-hop <claudia.i.hopkins@gmail.com>
2020-06-02 10:30:19 +0100

    Read only page added

--- app/routes.js
+++ app/routes.js
@@ -18,3 +18,23 @@ router.post('/approve-recomendation-answer', function (req, res) {
 })
 
 module.exports = router
+
+
+
+
+// Branching
+router.post('/approve-recomendation-answer-dv02', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let over18 = req.session.data['over-18']
+
+  if (over18 === 'false') {
+    res.redirect('dv02/approve-recommendation-no')
+  } else {
+    res.redirect('dv02/approve-recommendation-yes')
+  }
+})
+
+module.exports = router
