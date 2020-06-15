6906cc73e0a7266f9fb97d3e83836a70620923b8
claudia-hop <claudia.i.hopkins@gmail.com>
2020-06-15 16:08:50 +0100

    Manager page update

--- app/routes.js
+++ app/routes.js
@@ -11,11 +11,29 @@ router.post('/approve-recomendation-answer', function (req, res) {
   let over18 = req.session.data['over-18']
 
   if (over18 === 'false') {
-    res.redirect('dv02-ut/approve-recommendation-no')
+    res.redirect('dv03-ut/approve-recommendation-no')
   } else {
-    res.redirect('dv02-ut/approve-recommendation')
+    res.redirect('dv03-ut/approve-recommendation')
   }
 })
 
 module.exports = router
 
+
+
+// Branching
+router.post('/refuse-or-approve', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let over19 = req.session.data['how-contacted']
+
+  if (over19 === 'phone') {
+    res.redirect('dv03-ut/make-recommendation')
+  } else {
+    res.redirect('dv03-ut/make-recommendation-grant')
+  }
+})
+
+module.exports = router
